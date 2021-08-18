6f6d44fe04a144dd9be9c9ff6dd4d807eaddb9c4
tkygtr6 <tkygtr6@gmail.com>
2021-08-18 20:26:23 +0900

    install_utils.sh

---
+++ install_utils.sh
@@ -0,0 +1,7 @@
+sudo apt install git jq nmap htop vim curl wget unzip gcc
+
+# install netdata
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
+
