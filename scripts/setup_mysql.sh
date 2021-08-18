57533095732c123a812510ae7af2a13e8a69aaac
sudo gpasswd -a isucon sudo <isucon@ip-172-31-42-87.ap-northeast-1.compute.internal>
2021-08-18 13:34:20 +0000

    Update setup_mysql.sh

--- scripts/setup_mysql.sh
+++ scripts/setup_mysql.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 sudo apt install -y mysql-server mysql-client
+
+sudo -i
+cat << EOF | tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+general_log = 1
+general_log_file  = /var/log/mysql/mysql.log
+
+slow_query_log = 1
+long_query_time = 0
+slow_query_log_file = /var/log/mysql/mysql-slow.sql
+
+EOF
+
+sudo systemctl enable mysql
+sudo systemctl restart mysql
