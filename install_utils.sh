657dac025729aab0caf47323f69e905c95004f62
tkygtr6 <tkygtr6@gmail.com>
2021-08-18 20:53:00 +0900

    fix install_utils.sh

--- install_utils.sh
+++ install_utils.sh
@@ -1,4 +1,6 @@
-sudo apt install git jq nmap htop vim curl wget unzip gcc
+git pull
+
+sudo apt install -y git jq nmap htop vim curl wget unzip gcc
 
 # install netdata
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait
