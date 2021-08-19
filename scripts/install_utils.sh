cd96462e17a0a1e8f19ec976221ba0b385aeb50b
tkygtr6 <tkygtr6@gmail.com>
2021-08-19 15:42:29 +0900

    Update

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -1,7 +1,7 @@
 git pull
 
 sudo apt update
-sudo apt install -y git jq nmap htop vim curl wget unzip gcc
+sudo apt install -y git jq nmap htop vim curl wget unzip gcc python3-dev libmysqlclient-dev
 
 # install netdata
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait
