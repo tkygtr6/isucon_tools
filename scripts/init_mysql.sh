0f3742630b1c18ccbd69feac27a3e8617e604368
tkygtr6 <tkygtr6@gmail.com>
2021-08-20 00:22:13 +0900

    fix

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -5,4 +5,5 @@ CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON isuda.* TO 'isucon'@'%';
 CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
 GRANT ALL ON isuda.* TO 'isucon'@'localhost';
+CREATE DATABASE isuda;
 EOF
