e05fc838fd78d1a90c310714cc748c1318a59472
tkygtr6 <tkygtr6@gmail.com>
2021-08-20 01:46:12 +0900

    Update update_hosts

--- servers/update_hosts.sh
+++ servers/update_hosts.sh
@@ -2,6 +2,7 @@ cat /etc/hosts | grep -v \# | grep isucon > /tmp/hosts_isucon
 
 for server in isucon-server1 isucon-server2 isucon-server3 isucon-bench; do
 	scp /tmp/hosts_isucon $server:/tmp/
+    ssh $server "sudo hostname $server"
 	ssh $server "sudo cat /etc/hosts | grep -v isucon | sudo tee /tmp/hosts.bak"
 	ssh $server "sudo cat /tmp/hosts.bak /tmp/hosts_isucon | sudo tee /etc/hosts"
 done
