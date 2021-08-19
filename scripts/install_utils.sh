cf1643c9850d09ebf8d8b4b1e19d2cddff40b4a1
tkygtr6 <tkygtr6@gmail.com>
2021-08-20 00:21:25 +0900

    Update install_utils.sh

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -1,10 +1,12 @@
-git pull
-
 sudo apt update
-sudo apt install -y git jq nmap htop vim curl wget unzip gcc python3-dev libmysqlclient-dev
+sudo apt install -y git jq nmap htop vim curl wget unzip gcc python3-dev libmysqlclient-dev python3-dev libmysqlclient-dev fish
 
 # install netdata
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait
 sudo systemctl stop firewalld.service
 sudo systemctl disable firewalld.service
 
+# if installation of netdata fails
+# sudo add-apt-repository ppa:phusion.nl/misc && sudo apt update && sudo apt install netdata
+# sudo vim /etc/netdata/netdata.conf # change bind-address to 0.0.0.0
+# sudo systemctl enable netdata && sudo systemctl restart netdata
